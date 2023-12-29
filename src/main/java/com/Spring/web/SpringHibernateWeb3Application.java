a55c77cbc32a2dcad729adc8829d0ebe3712821e
Sharanvthu <104510061+Sharanvthu@users.noreply.github.com>
2023-12-29 10:59:02 +0530

    Add files via upload
    First commit

---
+++ src/main/java/com/Spring/web/SpringHibernateWeb3Application.java
@@ -0,0 +1,13 @@
+package com.Spring.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateWeb3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateWeb3Application.class, args);
+	}
+
+}
